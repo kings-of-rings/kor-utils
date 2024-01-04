9689bbe3fa3d29e50a918c8921491d8f2b57aa4c
XDapps <xdev@xdapps.xyz>
2024-01-03 21:41:23 -0500

    first commit

---
+++ src/utils/callProtectedLambda.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import axios from "axios";
+
+dotenv.config();
+
+export const callProtectedLambda = async (endpoint: string, apiKey: string, payload: Record<string, unknown>) => {
+  const result = await axios.post(endpoint, payload, {
+    headers: {
+      'Content-Type': `application/json;`,
+      Accept: '*/*',
+      AcceptEncoding: 'gzip, deflate, br',
+      Connection: 'keep-alive',
+      'x-api-key': apiKey
+    }
+  }
+  ).catch(function (error) {
+    console.log('Error ', error);
+  });
+  return result;
+}
