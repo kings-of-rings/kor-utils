162f63143582a3057df9c9753aa36a06a90f675b
XDapps <xdev@xdapps.xyz>
2024-02-12 13:53:37 -0500

    5

--- src/utils/callProtectedLambda.ts
+++ src/utils/callProtectedLambda.ts
@@ -4,18 +4,24 @@ import axios from "axios";
 dotenv.config();
 
 export const callProtectedLambda = async (endpoint: string, apiKey: string, payload: Record<string, unknown>) => {
-  const result = await axios.post(endpoint, payload, {
-    headers: {
-      'Content-Type': `application/json;`,
-      Accept: '*/*',
-      AcceptEncoding: 'gzip, deflate, br',
-      Connection: 'keep-alive',
-      'x-api-key': apiKey
-    }
-  }
-  ).catch(function (error) {
-    console.log('Error ', error);
-	  return error;
-  });
-  return result;
+	try {
+		const result = await axios.post(endpoint, payload, {
+			headers: {
+				'Content-Type': `application/json;`,
+				Accept: '*/*',
+				AcceptEncoding: 'gzip, deflate, br',
+				Connection: 'keep-alive',
+				'x-api-key': apiKey
+			}
+		}
+		).catch(function (error) {
+			//console.log('Error1 ', error);
+			return error;
+		});
+
+		return result;
+	} catch (error) {
+		console.log('Error2 ', error);
+		return error;
+	}
 }
